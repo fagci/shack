34bc18d7cabb1a0f78c73074e67d498c59ded7e9
Mikhail Yudin <fagci.nsk@gmail.com>
2022-01-15 02:19:06 +0700

    refactor

--- lib/net.sh
+++ lib/net.sh
@@ -3,7 +3,7 @@ function get_hostname() {
 }
 
 function check_port() {
-    timeout "${3:-1}" bash -c "echo > /dev/tcp/${1}/${2} && echo $2" 2>/dev/null
+    timeout "${3:-1}" bash -c "</dev/tcp/${1}/${2} && echo $2" 2>/dev/null
 }
 
 export -f get_hostname check_port

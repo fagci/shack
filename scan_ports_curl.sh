36273f80a196d6fdf06393fa8ed9e5a52365bc95
Mikhail Yudin <fagci.nsk@gmail.com>
2022-01-15 02:48:44 +0700

    feat: curl based port scanner

---
+++ scan_ports_curl.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+target="${1:-127.0.0.1}"
+
+export TIMEOUT=0.5
+
+function check_port_curl() {
+    curl -m "$TIMEOUT" --connect-timeout "$TIMEOUT" \
+        -svo /dev/null "${1}:${2}" 2>&1 \
+        | grep -Fq Connected
+}
+
+export -f check_port_curl
+
+seq "${2:-1}" "${3:-1024}" \
+    | xargs -P128 -I@ bash -c "check_port_curl ${target} @ && echo @"
